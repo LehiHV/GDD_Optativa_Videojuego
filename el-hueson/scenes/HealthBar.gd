965e853cd21fa17c4a98d66ccab4692942b52da6
Branyoe <branyoe.dev@gmail.com>
2024-11-25 16:46:25 -0600

    feat: add health-bar

---
+++ el-hueson/scenes/HealthBar.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var HealthLabel: Label
+var HealthBar: ProgressBar
+
+func _ready():
+	HealthLabel = get_node("HealthLabel")
+	HealthBar = get_node("HealthProgressBar")
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+	
+func set_health(value: int):
+	HealthLabel.text = "Health: %s" % value
+	HealthBar.value = value
+
+
--- el-hueson/scripts/player.gd
+++ el-hueson/scripts/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
+const MAX_HEALTH = 3
+
 @export var speed = 130
 @export var jump_force = -300
 @export var gravity = 800
+@export var health = MAX_HEALTH
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var health_bar = $"HealthBar"
 
 var audio_player: AudioStreamPlayer
 var death_sound: AudioStreamPlayer
 var jump_sound: AudioStreamPlayer
 var is_dead = false
+var death_animation_started = false
+
 
 func _ready() -> void:
 	audio_player = AudioStreamPlayer.new()
@@ -31,41 +37,55 @@ func _ready() -> void:
 	jump_sound.stream = jump_stream
 	add_child(jump_sound)
 	jump_sound.volume_db = 0
+	
+	health_bar.set_health(health)
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
+		if not death_animation_started:
+			sprite.play("death")
+			death_animation_started = true
 		return
-	
-	var direction = 0
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if position.y > 600:
-			die()
-	
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	elif Input.is_action_pressed("ui_right"):
-		direction = 1
-	
-	velocity.x = direction * speed
-	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = jump_force
-		jump_sound.play()
-	
-	move_and_slide()
-	
-	if not is_on_floor():
-		sprite.play("jump")
-	elif direction != 0:
-		sprite.play("walk")
-		sprite.flip_h = direction < 0
 	else:
-		sprite.play("idle")
+		var direction = 0
+		
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			if position.y > 600:
+				die()
+		
+		if Input.is_action_pressed("ui_left"):
+			direction = -1
+		elif Input.is_action_pressed("ui_right"):
+			direction = 1
+		
+		velocity.x = direction * speed
+		
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+			velocity.y = jump_force
+			damage(1)
+			jump_sound.play()
+		
+		move_and_slide()
+		
+		if not is_on_floor():
+			sprite.play("jump")
+		elif direction != 0:
+			sprite.play("walk")
+			sprite.flip_h = direction < 0
+		else:
+			sprite.play("idle")
+
+func damage(value: int) -> void:
+	health -= value
+	health_bar.set_health(health)
+	if health < 0:
+		die()
+		
 
 func die() -> void:
 	is_dead = true
 	audio_player.stop()
 	death_sound.play()
+	sprite.play("death")
 	velocity = Vector2.ZERO
